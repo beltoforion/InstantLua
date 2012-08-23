7ce3c66c37888880b1be354ce4ae6d2f46173c32
Ingo Berg <muparser@beltoforion.de>
2012-08-24 00:29:46 +0200

    removed 3rd party love archives

--- projects/AutoBenchmark/empty.lua
+++
@@ -1,9 +0,0 @@
------------------------------------------------------------------
--- The code below is the benchmarked code.                     --
--- This can be modified for your own benchmarks.               --
------------------------------------------------------------------
-
-empty = {name='Empty'}
-function empty.run() end
-function empty.update() end
-function empty.load() end
